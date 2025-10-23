3651a039b152aa33d705bac2f885ed0b873d37f6
Md Robiul Islam Sakib <70893762+sakib-maho@users.noreply.github.com>
2025-10-23 15:07:15 +0900

    Fix copyright and license issues
    Applied fixes:
    - Added LICENSE file
    - Added copyright headers to 1 files

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,3 +1,9 @@
+/**
+ * Copyright (c) 2025 sakib-maho
+ * Licensed under the MIT License
+ * See LICENSE file for details
+ */
+
 import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
